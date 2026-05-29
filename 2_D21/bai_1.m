a31dc4291db62882e4a0457412be5f550f50a6e3
BuiDiep205 <bdiep12122005@gmail.com>
2026-05-29 08:28:12 +0700

    de

--- 2_D21/bai_1.m
+++ 2_D21/bai_1.m
@@ -6,6 +6,7 @@ ts = 1/fs;
 F = 10;
 T = 1/F;
 t = 0:ts:10*T-ts;
+
 x = sin(8*pi*t);
 
 %a
@@ -13,27 +14,27 @@ f = 1200;
 a = 1;
 phic = pi/2;
 m = 0.5;
- 
-Am = max(abs(x)); %ép chỉ trong phạm vi (-1, 1)
+
+Am = max(abs(x));
 x_norm = x/Am;
 
 xc = a*cos(2*pi*f*t + phic);
-y = (1 + m*x_norm) .* xc;
+
+s = (1 + m*x_norm) .* xc;
 
 %b
 figure;
 subplot(3,1,1);
-plot(t, x, 'LineWidth',1.5);
+plot(t, x, 'LineWidth', 1.5);
+title('Tin hieu ban tin');
 grid on;
-title('Ban tin');
 
 subplot(3,1,2);
 plot(t, xc, 'LineWidth', 1.5);
+title('Tin hieu song mang');
 grid on;
-title('Song mang');
-xlim([0 10*T]);
 
 subplot(3,1,3);
-plot(t, y, 'LineWidth', 1.5);
+plot(t, s, 'LineWidth', 1.5);
+title('Tin hieu sau dieu bien');
 grid on;
-title('Sau dieu che');
---
+++ 2_D21/bai_3.m
@@ -0,0 +1,36 @@
+clc; clear;
+%a
+N_bits = 10000;
+bit_stream = randi([0, 1], 1, N_bits);
+
+%b
+M = 4;
+k = log2(M);
+SNR_dB = 10;
+
+bit_matrix = reshape(bit_stream, k, [])';
+symbols = bi2de(bit_matrix, 'left-msb');
+
+tx_signal = qammod(symbols, M, 'UnitAveragePower', true);
+rx_signal = awgn(tx_signal, SNR_dB, 'measured');
+
+rz_symbols = qamdemod(rx_signal, M, 'UnitAveragePower', true);
+rx_bit_matrix = de2bi(rz_symbols, k, 'left-msb');
+rx_bit_stream = reshape(rx_bit_matrix, 1, []);
+
+%c
+figure;
+subplot(1,2,1);
+plot(tx_signal, 'ro');
+title('Chom sao truoc kenh');
+grid on;
+
+subplot(1,2,2);
+plot(rx_signal, 'b.');
+title('Chom sao sau kenh');
+grid on;
+
+%d
+[num_errors, ber] = biterr(bit_stream, rx_bit_stream);
+disp(['So bit loi: ', num2str(num_errors)]);
+disp(['Ti le loi BER; ', num2str(ber)]);
